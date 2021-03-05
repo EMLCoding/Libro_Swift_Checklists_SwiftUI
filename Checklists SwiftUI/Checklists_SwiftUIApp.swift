a086d48fa9074445fd8f6153150f6fcbd57066c8
Edu-Estudios <eduardo.ml.trabajo@gmail.com>
2021-03-05 16:55:48 +0100

    Initial Commit

---
+++ Checklists SwiftUI/Checklists_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Checklists_SwiftUIApp.swift
+//  Checklists SwiftUI
+//
+//  Created by Eduardo Martin Lorenzo on 05/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct Checklists_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Checklists SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Checklists SwiftUI
+//
+//  Created by Eduardo Martin Lorenzo on 05/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
